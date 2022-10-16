d52a3b7e567e7cc12fb6cbb7be6f2c722378c616
nhatnv6767 <nhatnvse90183@gmail.com>
2022-10-17 02:34:23 +0900

    Administrator

--- src/main/java/Administrator.java
+++ src/main/java/Administrator.java
@@ -55,8 +55,106 @@ public class Administrator {
 
     // hien thi dat duoc giao dich trong thang 9/2013
     private void showListTransactionSoil(String Date) {
-
+        System.out.println("List of Transaction Soil");
+        if (this.listTransactionSoil.size() == 0) {
+            System.out.println("Do not have transaction.");
+            return;
+        }
+        String title = String.format("%s%15s%15s%15s%15s", "ID", "Date", "Unit Price", "Area", "Soil Type");
+        for (Transaction transaction : listTransactionSoil) {
+            if (checkDate(transaction.getDate(), Date)) {
+                System.out.println(transaction.toString());
+            }
+        }
     }
 
+    // hien thi gd nha trong thang 9/2013
+    private void showListTransactionHome(String Date) {
+        System.out.println("List of Transaction Home");
+        if (this.listTransactionSoil.size() == 0) {
+            System.out.println("Do not have transaction.");
+            return;
+        }
+        String title = String.format("%s%15s%15s%15s%15s%15s", "ID", "Date", "Unit Price", "Area", "Home Type", "Address");
+        for (Transaction transaction : listTransactionHome) {
+            if (checkDate(transaction.getDate(), Date)) {
+                System.out.println(transaction.toString());
+            }
+        }
+    }
+
+    // tinh trung binh tien gd dat
+    private double averageTransactionSoil() {
+        double sum = 0;
+        for (Transaction transaction : listTransactionSoil) {
+            sum += transaction.getPrice();
+        }
+        return sum / listTransactionSoil.size();
+    }
+
+    // tao menu
+    private void subMenu(int choice) {
+        if (choice == 1) {
+            System.out.println("1. Add Transaction Soil");
+            System.out.println("2. Add Transaction Home");
+        } else if (choice == 2) {
+            System.out.println("1. Show Transaction Soil");
+            System.out.println("2. Show Transaction Home");
+        }
+    }
+
+    public void menu() {
+        while (true) {
+            System.out.println("1. Add Transaction");
+            System.out.println("2. Show list transaction");
+            System.out.println("3. Show count of Transaction Soil and Home");
+            System.out.println("4. Average price of Transaction Soil");
+            System.out.println("5. Display transaction of 9/2013");
+            System.out.println("6. Exit");
+            int choice = Input.inputInt("Enter your choice: ");
+            menu(choice);
+        }
+    }
+
+    private void menu(int choice) {
+        int tempChoice = 0;
+        switch (choice) {
+            case 1:
+                subMenu(choice);
+                tempChoice = Input.inputInt("Enter your choice: ");
+                if (tempChoice == 1) {
+                    TransactionSoil t = TransactionSoil.inputTransactionSoil(listTransactionSoil.size() + 1);
+                    addTransactionSoil(t);
+                } else if (tempChoice == 2) {
+                    TransactionHome t = TransactionHome.inputTransactionHome(listTransactionHome.size() + 1);
+                    addTransactionHome(t);
+                }
+                break;
+            case 2:
+                subMenu(choice);
+                tempChoice = Input.inputInt("Enter your choice: ");
+                if (tempChoice == 1) {
+                    showListTransactionSoil();
+                } else if (tempChoice == 2) {
+                    showListTransactionHome();
+                }
+                break;
+
+            case 3:
+                System.out.println("Count of Transaction Soil: " + listTransactionSoil.size());
+                System.out.println("Count of Transaction Home: " + listTransactionHome.size());
+                break;
+            case 4:
+                System.out.println("Average price of Transaction Soil: " + averageTransactionSoil());
+                break;
+            case 5:
+                showListTransactionHome("9/2013");
+                showListTransactionSoil("9/2013");
+                break;
+            case 6:
+                System.exit(0);
+                break;
+        }
+    }
 
 }
